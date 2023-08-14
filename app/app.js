02951519631b018993f05cc98406f507598c3d26
Kumaravel187 <46121794+Kumaravel187@users.noreply.github.com>
2023-08-14 20:38:14 +0800

    Added in js
    - Added in vanilla js functionality to open and close accordion
    - Added in new scss classes
    - Modified scss styling for accordion

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
-console.log("Hi");
+const btns = document.querySelectorAll(".accordion__question");
+
+btns.forEach(function (btnItem) {
+  btnItem.addEventListener("click", function () {
+    btnItem.classList.toggle("open");
+    btnItem.nextElementSibling.classList.toggle("open");
+  });
+});
